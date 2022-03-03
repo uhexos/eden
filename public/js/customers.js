d618b789376f3af5743b82c0d08672c52fd687a8
Nwokorobia Ugochukwu <uhexos@gmail.com>
2022-03-03 23:54:20 +0100

    remove unneeded view

--- public/js/customers.js
+++ public/js/customers.js
@@ -13,7 +13,7 @@ let table = $('#customersTable').DataTable({
         {
             data: 'firstname',
             "render": function (data, type, row, meta) {
-                return '<a href="/customers/' + row['id'] + '">' + data + '</a>';
+                return '<a href="api/customer/' + row['id'] + '">' + data + '</a>';
             }
         },
         {
@@ -24,17 +24,17 @@ let table = $('#customersTable').DataTable({
     ]
 });
 
-let path = window.location.pathname.split('/');
+// let path = window.location.pathname.split('/');
 
-if (path[2]) {
-    axios.get('/api/customer/'+path[2])
-        .then(function (response) {
-            document.getElementById('name').value = response.data.firstname + response.data.lastname
-            document.getElementById('gardener').value = response.data.gardeners[0].firstname
-            document.getElementById('location').value = response.data.location.name
-            document.getElementById('country').value = response.data.country.name
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
+// if (path[2]) {
+//     axios.get('/api/customer/'+path[2])
+//         .then(function (response) {
+//             document.getElementById('name').value = response.data.firstname + response.data.lastname
+//             document.getElementById('gardener').value = response.data.gardeners[0].firstname
+//             document.getElementById('location').value = response.data.location.name
+//             document.getElementById('country').value = response.data.country.name
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// }
