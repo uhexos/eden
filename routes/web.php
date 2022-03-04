81ecf813263845daf12401d4337d71e29c9d60cb
Nwokorobia Ugochukwu <uhexos@gmail.com>
2022-03-04 08:43:49 +0100

    login naming

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login')->name('login');
-});
+    return view('login');
+})->name('login');
 Route::get('/register', function () {
     return view('register');
 });
